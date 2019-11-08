dcccdffb172996edbf34c2df153cb40dd1d0f4ec
PJungeun <719wjddms@naver.com>
2019-11-08 12:52:20 +0900

    1108

--- TOUCH/urls.py
+++ TOUCH/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 import count.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', count.views.main, name="main"),
     path('count/',count.views.count, name ="count"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
